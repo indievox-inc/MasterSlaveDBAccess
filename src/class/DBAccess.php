00adf87322017630f8071ddf476f622cc8f4c516
Fukuball Lin <fukuball@gmail.com>
2016-03-22 20:35:51 +0800

    增加成員變數

--- src/class/DBAccess.php
+++ src/class/DBAccess.php
@@ -27,4 +27,14 @@ namespace iNDIEVOX\DBAccess;
 class DBAccess
 {
 
+    protected static $db_obj;
+    protected static $instance_count = 0;
+    public           $current_mode;
+    public           $context_status;
+    protected        $db_name;
+    protected        $db_connection;
+
+    protected        $db_name_poll;
+    protected        $db_connection_poll;
+
 }// end of class DBAccess
