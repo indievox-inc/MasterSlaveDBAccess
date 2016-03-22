5f2555d0d53e5e4a31c56844d7804fc193df5e89
Fukuball Lin <fukuball@gmail.com>
2016-03-22 21:43:05 +0800

    add connectMaster and connectSlave method

--- src/class/DBAccess.php
+++ src/class/DBAccess.php
@@ -166,4 +166,109 @@ class DBAccess
         self::$instance_count++;
 
     }// end function __construct
+
+    /**
+     * Method connectMaster
+     *
+     * @return void
+     */
+    public function connectMaster()
+    {
+
+        // connect master
+        $m_db_host       = $this->db_config['database_server']['master']['db_host'];
+        $m_db_name       = $this->db_config['database_server']['master']['db_name'];
+        $m_db_user       = $this->db_config['database_server']['master']['db_user'];
+        $m_db_password   = $this->db_config['database_server']['master']['db_password'];
+
+        try {
+
+            $this->db_connection_poll['master']
+                = new PDO(
+                    'mysql:host=' . $m_db_host . ';dbname=' . $m_db_name,
+                    $m_db_user,
+                    $m_db_password
+                );
+
+            $this->db_name_poll['master'] = $m_db_name;
+            $this->db_connection_poll['master']->query("SET NAMES UTF8");
+
+            // switch to master
+            $this->current_mode  = 'master';
+            $this->db_name       = $this->db_name_poll['master'];
+            $this->db_connection = $this->db_connection_poll['master'];
+
+        } catch (PDOException $e) {
+
+            throw new RuntimeException();
+
+        }
+
+    }// end function connectMaster
+
+    /**
+     * Method connectSlave
+     *
+     * @param array $options['mode'] # input options
+     *
+     * @return void
+     */
+    public function connectSlave($options=array())
+    {
+
+        $defaults = array('mode'=>'random');
+
+        $options = array_merge($defaults, $options);
+
+        if (!empty($this->db_config["slave_database_name"])) {
+
+            // connect slave
+            switch ($options['mode']) {
+            case 'random':
+                $slave_db_choose
+                    = $this->db_config["slave_database_name"][array_rand($this->db_config["slave_database_name"])];
+                break;
+            default:
+                $slave_db_choose
+                    = $options['mode'];
+                break;
+            }
+
+
+            $s_db_host       = $this->db_config['database_server'][$slave_db_choose]['db_host'];
+            $s_db_name       = $this->db_config['database_server'][$slave_db_choose]['db_name'];
+            $s_db_user       = $this->db_config['database_server'][$slave_db_choose]['db_user'];
+            $s_db_password   = $this->db_config['database_server'][$slave_db_choose]['db_password'];
+
+            try {
+
+                $this->db_connection_poll[$slave_db_choose]
+                    = new PDO(
+                        'mysql:host=' . $s_db_host . ';dbname=' . $s_db_name,
+                        $s_db_user,
+                        $s_db_password
+                    );
+
+                $this->db_name_poll[$slave_db_choose] = $s_db_name;
+                $this->db_connection_poll[$slave_db_choose]->query("SET time_zone='+8:00'");
+                $this->db_connection_poll[$slave_db_choose]->query("SET NAMES UTF8");
+
+                // switch to slave
+                $this->current_mode  = $slave_db_choose;
+                $this->db_name       = $this->db_name_poll[$slave_db_choose];
+                $this->db_connection = $this->db_connection_poll[$slave_db_choose];
+
+            } catch (PDOException $e) {
+
+                throw new RuntimeException();
+
+            }
+
+        } else {
+
+            $this->connectMaster();
+
+        }
+
+    }// end function connectSlave
 }// end of class DBAccess
--- test/DBAccessTest.php
+++ test/DBAccessTest.php
@@ -4,9 +4,47 @@ use iNDIEVOX\DBAccess\DBAccess;
 class DBAccessTest extends PHPUnit_Framework_TestCase
 {
 
-    public function testExample()
+    protected static $db_config;
+
+    public static function setUpBeforeClass()
     {
-        $this->assertEquals('test', 'test');
+        self::$db_config = array(
+            "database_server" => array(
+                "master"=>array(
+                    "db_host"=>'localhost',
+                    "db_name"=>'homestead',
+                    "db_user"=>'root',
+                    "db_password"=>''
+                ),
+                "slave1"=>array(
+                    "db_host"=>'localhost',
+                    "db_name"=>'homestead',
+                    "db_user"=>'root',
+                    "db_password"=>''
+                ),
+                "slave2"=>array(
+                    "db_host"=>'localhost',
+                    "db_name"=>'homestead',
+                    "db_user"=>'root',
+                    "db_password"=>'',
+                )
+            ),
+            "slave_database_name" => array(
+                'slave1',
+                'slave2'
+            )
+        );
+    }
+
+    public static function tearDownAfterClass()
+    {
+        self::$db_config = NULL;
+    }
+
+    public function testGetInstance()
+    {
+        $db_obj = DBAccess::getInstance(self::$db_config);
+        $this->assertEquals($db_obj->context_status, 'one_time');
     }
 
 }
