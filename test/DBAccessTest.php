5b2793e7cfc42e0029e0648073730fc799bbb264
Fukuball Lin <fukuball@gmail.com>
2016-03-22 20:28:11 +0800

    Add sample test

--- test/DBAccessTest.php
+++ test/DBAccessTest.php
@@ -4,4 +4,9 @@ use iNDIEVOX\DBAccess\DBAccess;
 class DBAccessTest extends PHPUnit_Framework_TestCase
 {
 
+    public function testExample()
+    {
+        $this->assertEquals('test', 'test');
+    }
+
 }
