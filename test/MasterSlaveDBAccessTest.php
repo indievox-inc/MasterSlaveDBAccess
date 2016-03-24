b438258c6d12a53d537305b7ba0f47fc887c8409
Fukuball Lin <fukuball@gmail.com>
2016-03-25 01:17:59 +0800

    remove unused code

--- test/MasterSlaveDBAccessTest.php
+++ test/MasterSlaveDBAccessTest.php
@@ -10,28 +10,6 @@ class MasterSlaveDBAccessTest extends PHPUnit_Framework_TestCase
     public static function setUpBeforeClass()
     {
 
-        $create_user_table_sql = "CREATE TABLE IF NOT EXISTS `user` (".
-                            "`id` int(11) unsigned NOT NULL AUTO_INCREMENT,".
-                            "`path` char(30) NOT NULL,".
-                            "`is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',".
-                            "`create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
-                            "`modify_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
-                            "`delete_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
-                            "PRIMARY KEY (`id`)".
-                        ") ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-
-        $drop_user_table_sql = "DROP TABLE user";
-
-        $insert_user_sql = "INSERT INTO `user` (`path`, `is_deleted`, `create_time`, `modify_time`, `delete_time`) VALUES".
-                                "('fukuball', 0, '2016-12-30 00:00:00', '2016-12-30 16:12:18', '0000-00-00 00:00:00');";
-
-        $select_user_sql = "SELECT * FROM user WHERE `path`='fukuball' ";
-
-        $update_user_sql = "UPDATE `user` SET `path`='fukuball-lin' WHERE `path`='fukuball' ";
-
-        $delete_user_sql = "DELETE `user` WHERE `path`='fukuball' ";
-
-
         self::$db_config = array(
             "database_server" => array(
                 "master"=>array(
