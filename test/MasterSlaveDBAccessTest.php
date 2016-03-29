444b4645bc0f662c84fe10d51698cf91341d4f15
Fukuball Lin <fukuball@gmail.com>
2016-03-29 15:46:01 +0800

    add delete test case

--- test/MasterSlaveDBAccessTest.php
+++ test/MasterSlaveDBAccessTest.php
@@ -215,9 +215,7 @@ class MasterSlaveDBAccessTest extends PHPUnit_Framework_TestCase
 
         $db_obj = MasterSlaveDBAccess::getInstance(self::$db_config);
         MasterSlaveDBAccess::forceSwitchMaster();
-        $insert_id = $db_obj->insertCommand(
-            $insert_sql, $param
-        );
+        $insert_id = $db_obj->insertCommand($insert_sql, $param);
         $this->assertEquals('1', $insert_id);
         unset($db_obj);
         MasterSlaveDBAccess::destroyInstance();
@@ -243,9 +241,7 @@ class MasterSlaveDBAccessTest extends PHPUnit_Framework_TestCase
 
         $db_obj = MasterSlaveDBAccess::getInstance(self::$db_config);
         MasterSlaveDBAccess::forceSwitchMaster();
-        $insert_id = $db_obj->insertCommand(
-            $insert_sql, $param
-        );
+        $insert_id = $db_obj->insertCommand($insert_sql, $param);
         $this->assertEquals('1', $insert_id);
 
         $select_sql = "SELECT * FROM user WHERE `id`=:id ";
@@ -281,9 +277,7 @@ class MasterSlaveDBAccessTest extends PHPUnit_Framework_TestCase
 
         $db_obj = MasterSlaveDBAccess::getInstance(self::$db_config);
         MasterSlaveDBAccess::forceSwitchMaster();
-        $insert_id = $db_obj->insertCommand(
-            $insert_sql, $param
-        );
+        $insert_id = $db_obj->insertCommand($insert_sql, $param);
         $this->assertEquals('1', $insert_id);
 
         $update_sql = "UPDATE `user` SET `path`='fukuball-lin' WHERE `id`=:id ";
@@ -293,7 +287,7 @@ class MasterSlaveDBAccessTest extends PHPUnit_Framework_TestCase
         );
 
         MasterSlaveDBAccess::forceSwitchMaster();
-        $affected_rows = $db_obj->updateCommand($update_sql);
+        $affected_rows = $db_obj->updateCommand($update_sql, $param);
         $this->assertEquals(1, $affected_rows);
 
         $select_sql = "SELECT * FROM user WHERE `id`=:id ";
@@ -310,5 +304,57 @@ class MasterSlaveDBAccessTest extends PHPUnit_Framework_TestCase
 
     }
 
+    public function testDeleteCommand()
+    {
 
+        $insert_sql = "INSERT INTO `user` ".
+            "(`id`, `path`, `is_deleted`, `create_time`, "."`modify_time`, `delete_time`) ".
+            "VALUES ".
+            "(:id, :path, :is_deleted, :create_time, :modify_time, :delete_time);";
+
+        $param = array(
+            ":id"           => '1',
+            ":path"         => 'fukuball',
+            ":is_deleted"   => '0',
+            ":create_time"  => '2016-12-30 00:00:00',
+            ":modify_time"  => '2016-12-30 16:12:18',
+            ":delete_time"  => '0000-00-00 00:00:00'
+        );
+
+        $db_obj = MasterSlaveDBAccess::getInstance(self::$db_config);
+        MasterSlaveDBAccess::forceSwitchMaster();
+        $insert_id = $db_obj->insertCommand($insert_sql, $param);
+        $this->assertEquals('1', $insert_id);
+
+        $select_sql = "SELECT * FROM user WHERE `id`=:id ";
+
+        $param = array(
+            ":id" => '1'
+        );
+
+        MasterSlaveDBAccess::forceSwitchMaster();
+        $query_result = $db_obj->selectCommand($select_sql, $param);
+        $this->assertEquals('1', $query_result[0]["id"]);
+
+        $delete_sql = "DELETE `user` WHERE `id`=:id ";
+
+        $param = array(
+            ":id" => '1'
+        );
+
+        MasterSlaveDBAccess::forceSwitchMaster();
+        $affected_rows = $db_obj->deleteCommand($delete_sql, $param);
+        $this->assertEquals(1, $affected_rows);
+
+        $select_sql = "SELECT * FROM user WHERE `id`=:id ";
+
+        $param = array(
+            ":id" => '1'
+        );
+
+        MasterSlaveDBAccess::forceSwitchMaster();
+        $query_result = $db_obj->selectCommand($select_sql, $param);
+        $this->assertEquals(0, count($query_result));
+
+    }
 }
